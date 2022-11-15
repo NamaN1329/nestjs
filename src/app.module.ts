4f1b2813a7e5a2de5921ee5bb88ee03fb2fdb61a
NamaN1329 <namanthapa1212@gmail.com>
2022-11-15 07:23:05 +0530

    feat:connect mongodb in nestjs with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      username: 'TestApps',
+      password: '',
+      database: 'testApps',
+      entities: [User],
+      synchronize: true,
+      useUnifiedTopology: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
